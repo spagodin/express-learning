f858a02bc292f4e031cbf55919418fc2324a179d
Scott Pagodin <seppy101@gmail.com>
2018-03-03 12:31:33 -0500

    Skeleton Plumbing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/routes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.set('view engine', 'pug');
+
+app.use(routes);
 
 app.listen(3000, () => console.log('App is running on port 3000'));
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('./pages/home')
+});
+
+router.use('/beer', require('./beer'));
+router.use('/weather', require('./weather'));
+
+module.exports = router;
