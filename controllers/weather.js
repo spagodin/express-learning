573fc15d6920777e3b2c160521291ed43d750c01
Scott Pagodin <seppy101@gmail.com>
2018-03-04 11:12:59 -0500

    Added weather route and controller for static city location

---
+++ controllers/weather.js
@@ -0,0 +1,20 @@
+const config = require('../config/config');
+const axios = require('axios');
+
+
+exports.getWeather = (req, res) => {
+  let api_key = config.owm_api_key
+  let city = 'knoxville'
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${api_key}`
+
+  return axios.get(url)
+  .then( res => {
+    return {
+      city : res.data.name,
+      temp : res.data.main.temp
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
--- routes/weather.js
+++ routes/weather.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const weather = require('../controllers/weather');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('./pages/weather')
+  //getWeather returns a promise so I need to handle it.
+  let data = weather.getWeather()
+    .then( data => {
+      res.render('./pages/weather', {
+        city: data.city, 
+        temp: data.temp
+      });
+      //logging just to make sure I have the data
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  
 })
 
 module.exports = router;
