b15aa8e442a6d4f88ce8db73e0b3d7c533971a1d
Scott Pagodin <seppy101@gmail.com>
2018-03-06 16:01:05 -0500

    refactoring weather

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const routes = require('./routes/routes');
+const routes = require("./routes/routes");
 
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "pug");
 
 app.use(routes);
 
-app.listen(3000, () => console.log('App is running on port 3000'));
+app.listen(3000, () => console.log("App is running on port 3000"));
--- routes/weather.js
+++ routes/weather.js
@@ -3,19 +3,21 @@ const weather = require("../controllers/weather");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  //getWeather returns a promise so I need to handle it.
+  res.render("./pages/weather");
+});
+
+router.post("/", (req, res) => {
+  let cityName = req.body.city;
   weather
-    .getWeather()
+    .getWeather(cityName)
     .then(wdata => {
       res.render("./pages/weather", {
         city: wdata.city,
         temp: wdata.temp
       });
-      //logging just to make sure I have the data
-      console.log(wdata);
     })
     .catch(err => {
-      console.log(err);
+      res.send(err);
     });
 });
 
